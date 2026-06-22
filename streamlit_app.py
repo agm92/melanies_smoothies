99f839c311a6cf2ea58e656c09d1df5ef47e4889
4dri4n0 <105928561+agm92@users.noreply.github.com>
2026-06-22 13:39:51 +0200

    smoothiefroot url correction
    smoothiefroot url correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 #New section to display smoothiefroot nutrition information
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response.json())
 
 ingredients_list = st.multiselect(
