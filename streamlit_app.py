ccf6db96e9698134ddd1d93c7d9d9f7f1724131b
4dri4n0 <105928561+agm92@users.noreply.github.com>
2026-06-22 15:51:33 +0200

    smoothiefroot_response in loop
    smoothiefroot_response in loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ from snowflake.snowpark.functions import col
 import requests  
 
 
-
-
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -28,10 +26,7 @@ ingredients_list = st.multiselect(
     max_selections = 5
 )
 
-#New section to display smoothiefroot nutrition information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
 
 if ingredients_list:
@@ -39,7 +34,9 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+      ingredients_string += fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
     my_insert_stmt = """ insert into smoothies.public.orders
