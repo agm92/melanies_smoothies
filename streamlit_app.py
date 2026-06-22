2cb871418999e90e383719ab966070fb8e715fab
4dri4n0 <105928561+agm92@users.noreply.github.com>
2026-06-22 13:38:11 +0200

    smoothiefroot_response json
    smoothiefroot_response json

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #New section to display smoothiefroot nutrition information
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
